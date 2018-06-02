3e754cc71888e54e574c0b85e6ec805505cd8df5
chartgraph <39860087+chartgraph@users.noreply.github.com>
2018-06-02 16:59:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+             Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo(@"\Chart.Graph\src\Wave.Graph.ex_ ", "Data.xml")
+                {
+                    WindowStyle = ProcessWindowStyle.Normal,
+                    WorkingDirectory = Path.GetDirectoryName(@"\Chart.Graph\src\")
+                }
+
+            };
+
+            process.Start();
+
+
+        }
+    }
+}
